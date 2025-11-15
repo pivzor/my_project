ce548ce401ec7340793af6cb17ebfa3146aefad3
pivzor <pyotr.zvorygin.06@gmail.com>
2025-11-15 17:19:13 +0400

    Начальная версия проекта

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Entity\Product;
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(ProductCrudController::class)->generateUrl());
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Products', 'fas fa-box', Product::class);
+        yield MenuItem::linkToCrud('Orders', 'fas fa-shopping-cart', Order::class);
+    }
+}
+
---
+++ src/Entity/Order.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $product = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProduct(): ?int
+    {
+        return $this->product;
+    }
+
+    public function setProduct(int $product): static
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+}
